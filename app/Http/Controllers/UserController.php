947aa7225283ea2abf228efa33f388c4f880f2ba
Matthew Greco <mgreco2112@tutanota.com>
2025-09-18 14:47:18 -0400

    added login/logout functions, multi-state view of home based on Auth state, and create Post system

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function register(Request $request) {
         $incomingFields = $request->validate([
-            'name' => ['required', 'min:3', 'max:10'],
-            'email' => ['required', 'email'],
+            'name' => ['required', 'min:3', 'max:10', Rule::unique('users', 'name')],
+            'email' => ['required', 'email', Rule::unique('users','email')],
             'password' => ['required', 'min:8', "max:200"]
         ]);
 
         $incomingFields["password"] = bcrypt($incomingFields["password"]);
-        User::create($incomingFields);
+        $user = User::create($incomingFields);
+        Auth::login($user);
 
-        return 'Hello from our controller';
+        return redirect("/");
+    }
+
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginname' => ['required'],
+            'loginpassword' => ['required']    
+        ]);
+        
+
+        if (Auth::attempt(['name' => $incomingFields['loginname'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+        }
+
+        return redirect('/');
+    }
+
+    public function logout() {
+        Auth::logout();
+
+        return redirect("/");
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,43 @@
     <title>Document</title>
 </head>
 <body>
-    <div style="border: 3px solid black;">
-        <h2>Register</h2>
-        <form action="/register" method="POST">
+    @auth
+        <p>Congrats you are logged in.</p>
+        <form action="/logout" method="POST">
             @csrf
-            <input name="name" type="text" placeholder="name">
-            <input name="email" type="text" placeholder="email">
-            <input name="password" type="password" placeholder="password">
-            <button>Register</button>
+            <button>Log Out</button>
         </form>
-    </div>
+
+        <div style="border: 3px solid black">
+            <h2>Create a New Post</h2>
+            <form action="/create-post" method="POST">
+                @csrf
+                <input type="text" name="title" placeholder="title">
+                <textarea name="body" placeholder="body content..."></textarea>
+                <button>Save Post</button>
+            </form>
+        </div>
+    @else
+        <div style="border: 3px solid black;">
+            <h2>Register</h2>
+            <form action="/register" method="POST">
+                @csrf
+                <input name="name" type="text" placeholder="name">
+                <input name="email" type="text" placeholder="email">
+                <input name="password" type="password" placeholder="password">
+                <button>Register</button>
+            </form>
+        </div>
+        <div style="border: 3px solid black;">
+            <h2>Login</h2>
+            <form action="/login" method="POST">
+                @csrf
+                <input name="loginname" type="text" placeholder="name">
+                <input name="loginpassword" type="password" placeholder="password">
+                <button>Log in</button>
+            </form>
+        </div>
+    @endauth
+
 </body>
 </html>
