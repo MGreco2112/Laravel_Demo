a39bb03f1eb9860372444ad3c510c08bfad9d2ca
Matthew Greco <mgreco2112@tutanota.com>
2025-09-18 15:35:33 -0400

    added user field access to Posts via function, added username display to Posts in home

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,4 +23,37 @@ class PostController extends Controller
 
         return redirect('/');
     }
+
+    public function showEditScreen(Post $post) {
+        if (Auth::id() === $post->user_id) {
+            return view('edit-post', ['post' => $post]);
+        } else {
+            return redirect('/');
+        }
+    }
+
+    public function sendEdit(Post $post, Request $request) {
+        if (Auth::id() !== $post->user_id) {
+            return redirect('/');
+        }
+
+        $incomingFields = $request->validate([
+            'title'=> 'required',
+            'body'=> 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        $post->update($incomingFields);
+        return redirect('/');
+    }
+
+    public function deletePost(Post $post) {
+        if (Auth::id() === $post->user_id) {
+            $post->delete();
+        }
+        
+        return redirect('/');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,27 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
+
+// Basic Routing
 
 Route::get('/', function () {
-    return view('home');
+    // $posts = Post::all(); //get all posts from db
+    // $posts = Post::where('user_id', Auth::id())->get(); //get posts where user_id in post == the id of user in Auth
+
+    $posts = [];
+
+    if (Auth::check()) {
+        $posts = Auth::user()->posts()->latest()->get(); //get latest posts from current logged in user via Auth
+    }
+
+    return view('home', ['posts' => $posts]);
 });
+
+// User Controller
 
 Route::post('/register', [UserController::class, 'register']);
 
@@ -14,4 +29,12 @@ Route::post('/logout', [UserController::class, 'logout']);
 
 Route::post('/login', [UserController::class, 'login']);
 
+// Post Controller
+
 Route::post('/create-post', [PostController::class, 'createPost']);
+
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+
+Route::put('/edit-post/{post}', [PostController::class, 'sendEdit']);
+
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
